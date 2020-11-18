c8ab9e7ddbaaaed0596dc228b5de2471f42084ae
EPE <pech.erwan@hotmail.com>
2020-11-18 19:01:35 +0100

    Ajout du son avec les scènes

--- FPSZombie/Start2.gd
+++ FPSZombie/Start2.gd
@@ -1,11 +1,3 @@
 extends TextureRect
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-func _input(ev):
-	if Input.is_key_pressed(KEY_ENTER):
-		get_tree().change_scene("res://Terrain_jeux.tscn")
