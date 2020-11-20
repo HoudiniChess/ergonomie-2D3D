45204aaf356fa5d6853ce1c736115687a35f2fa6
EPE <pech.erwan@hotmail.com>
2020-11-20 08:22:31 +0100

    Dernière modif

--- FPSZombie/Page accueil.gd
+++ FPSZombie/Page accueil.gd
@@ -2,10 +2,10 @@ extends Control
 
 onready var audio = $Audio
 
-func _ready(): 
-	audio.play()
+#func _ready(): 
+	#audio.play()
 
 func _input(ev):
 	if Input.is_key_pressed(KEY_ENTER):
-		audio.stop()
+		#audio.stop()
 		get_tree().change_scene("res://Terrain_jeux.tscn")
